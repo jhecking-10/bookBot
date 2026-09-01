5573c24fcb7a542448e1ea35079b390abbb0e915
jhecking-10 <jhecking_10@protonmail.com>
2026-09-02 01:50:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,17 +18,20 @@ counted_chars = count_characters(book)
 sorted_list = chars_dict_to_sorted_list(counted_chars)
 
 def print_report(path: str, words: int, list: list[tuple[str, int]]):
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {path}...")
+    print("============ BOOKBOT ============\n")
+    print(f"Analyzing book found at {path}...\n")
     print("----------- Word Count ----------")
     # Loops over every tuple in the sorted list of characters, then omits spaces, punctuation, etc.
-    print(f"Found {words} total words")
+    print(f"Found {words} total words\n")
+    print("----------- Character Count ----------")
     for item in list:
         if item[0].isalpha():
             print(f"{item[0]}: {item[1]}")
-    print("============= END ===============")
+    print("\n============= END ===============")
 
 def main():
     print_report(sys.argv[1], word_count, sorted_list)
 
-main()
+
+if __name__ == "__main__":
+    main()
